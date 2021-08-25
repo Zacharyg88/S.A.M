a962f8e11b799956ca51308facec479e0326662c
Zach <zacharyg88@gmail.com>
2021-08-25 14:27:21 -0400

    added view controllers

---
+++ ViewControllers/MainMenuViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainMenuViewController.swift
+//  S.A.M
+//
+//  Created by Zach Eidenberger on 8/25/21.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ViewControllers/SWMissionLogViewController.swift
@@ -0,0 +1,52 @@
+//
+//  SWMissionLogViewController.swift
+//  SWTestApp
+//
+//  Created by Zach Eidenberger on 8/21/21.
+//
+
+import UIKit
+
+class SWMissionLogViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+
+    @IBOutlet weak var tableView: UITableView?
+    var missions: [MissionModel] = [] {
+        didSet {
+            self.tableView?.reloadData()
+        }
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        databaseManager.getAllMissionsFromDatabase { (missions, error) in
+            if error != nil {
+                print("Unable to get missions from database")
+            }else {
+                if let missionArray = missions {
+                    self.missions = missionArray
+                }
+            }
+        }
+        tableView?.register(UINib(nibName: "SWMissionLogTableViewCell", bundle: nil), forCellReuseIdentifier: "SWMissionLogTableViewCell")
+        tableView?.delegate = self
+        tableView?.dataSource = self
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return missions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell: SWMissionLogTableViewCell = tableView.dequeueReusableCell(withIdentifier: "SWMissionLogTableViewCell", for: indexPath) as? SWMissionLogTableViewCell {
+            cell.mission = missions[indexPath.row]
+            
+            return cell
+        }
+        return UITableViewCell()
+    }
+
+}
